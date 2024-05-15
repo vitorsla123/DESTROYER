12a04458c8fa16ed76f610944303d6af5af8aff4
R3TH PRIV <119117781+R3TH-PRIV@users.noreply.github.com>
2024-05-15 21:51:47 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -9,131 +9,139 @@
         pethicial
 ]]
 
+--------------------------------------------------------------------------------------R3THPRIV----------------------------------------------------------------------------------------
 local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
-local StarterGui = game:GetService("StarterGui")
 
-function sendnotification(notificationmessage)
-    if WVryGeXr38ZZtdJWtrBtyeEKdm9Kkweaxm7tnUpuCcH835AQN2aLxV2NeG76kYZuWnCZz4yRr == true then
-        StarterGui:SetCore("SendNotification", {
+function sendnotification(notfiicationmessage)
+    if R3THDEVICE == "Mobile" then
+        game:GetService("StarterGui"):SetCore("SendNotification", {
             Title = "R3TH PRIV";
-            Text = notificationmessage;
+            Text = notfiicationmessage;
             Duration = 5;
         })
     else
         Notification:Notify(
-            {Title = "R3TH PRIV", Description = notificationmessage},
+            {Title = "R3TH PRIV", Description = notfiicationmessage},
             {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "default"}
         )
     end
 end
 
-if zac5mDuh2AXjsBebYVg6ZSMEf4yxQkw8dRLNGKHF7J9CPT3qWp then
+if r3thexecuted then
     print("[ R3TH PRIV ]: Script already executed, if your having any problems join discord.gg/pethicial for support.")
     sendnotification("Script already executed, if your having any problems join discord.gg/pethicial for support.")
     return
 end
 
-getgenv().zac5mDuh2AXjsBebYVg6ZSMEf4yxQkw8dRLNGKHF7J9CPT3qWp = true
+getgenv().r3thexecuted = true
 
-wait()
-print("[ R3TH PRIV ]: R3TH PRIV LOADING...")
+--------------------------------------------------------------------------------------AUTOLOAD----------------------------------------------------------------------------------------
+if not isfolder("R3TH PRIV") then
+    makefolder("R3TH PRIV")
+end
+
+function loadr3th()
+    print("[ R3TH PRIV ]: Script loading, this may take awhile depending on your device.")
+    sendnotification("Script loading, this may take awhile depending on your device.")
+    if game.PlaceId == (142823291 or 335132309 or 636649648) then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Murder%20Mystery%202.lua'))()
+    elseif game.PlaceId == 7863262550 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/FOBLOX.lua'))()
+    elseif game.PlaceId == 5244411056 then
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Bloxy%20Bingo.lua'))()
+    elseif game.PlaceId == 8908228901 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/SharkBite%202.lua'))()
+    elseif game.PlaceId == 6847090259 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Bulked%20Up.lua'))()
+    elseif game.PlaceId == 12151453277 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Abyss%20World.lua'))()
+    elseif game.PlaceId == 2693739238 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/THIEF%20LIFE%20Simulator.lua'))()
+    elseif game.PlaceId == 3891618314 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Harbor%20Havoc.lua'))()
+    elseif game.PlaceId == 13772394625 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Blade%20Ball.lua'))()
+    elseif game.PlaceId == 14732610803 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Blade%20Ball.lua'))()
+    elseif game.PlaceId == 4889315193 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Total%20Roblox%20Drama.lua'))()
+    elseif game.PlaceId == 4939362930 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Total%20Roblox%20Drama.lua'))()
+    elseif game.PlaceId == 4939362610 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Total%20Roblox%20Drama.lua'))()
+    elseif game.PlaceId == 7172152072 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Total%20Roblox%20Drama.lua'))()
+    elseif game.PlaceId == 1962086868 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Tower%20of%20Hell.lua'))()
+    elseif game.PlaceId == 3582763398 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Tower%20of%20Hell.lua'))()
+    elseif game.PlaceId == 5253186791 then
+        print("[ R3TH PRIV ]: Game Supported!")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Tower%20of%20Hell.lua'))()
+    else
+        print("[ R3TH PRIV ]: Game not Supported.")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Universal.lua'))()
+    end
+    return
+end
 
 local UIS = game:GetService("UserInputService")
 local PC, PCwithTouch, Touchscreen = (UIS.MouseEnabled and UIS.KeyboardEnabled), (UIS.MouseEnabled and UIS.KeyboardEnabled and UIS.TouchEnabled), UIS.TouchEnabled
 if Touchscreen == true then
-    device = "Mobile"
-    getgenv().WVryGeXr38ZZtdJWtrBtyeEKdm9Kkweaxm7tnUpuCcH835AQN2aLxV2NeG76kYZuWnCZz4yRr = true
+    getgenv().R3THMOBILE = true
 else
-    device = "PC"
-    getgenv().WVryGeXr38ZZtdJWtrBtyeEKdm9Kkweaxm7tnUpuCcH835AQN2aLxV2NeG76kYZuWnCZz4yRr = false
+    getgenv().R3THMOBILE = false
 end
 
-print("[ R3TH PRIV ]: " ..device .." detected")
+print("[ R3TH PRIV ]: " ..R3THDEVICE .." detected")
 
-sendnotification("Script loading, this may take awhile depending on your device.")
-
-function getexploit()
-    local exploit =
-        (KRNL_LOADED and "Krnl") or
-        (IsElectron and "Electron") or
-        (Fluxus and "Fluxus") or
-        ("Unsupported")
-  
-    return exploit
+if isfile("R3TH PRIV/Executor.txt") then
+    getgenv().R3THEXECUTOR = readfile("R3TH PRIV/Executor.txt")
+    print(R3THEXECUTOR)
+    loadr3th()
+    return
 end
 
-print("[ R3TH PRIV ]: " ..getexploit() .." detected")
-if getexploit() == "Unsupported" then
-    print("[ R3TH PRIV ]: Unsupported executor detected, some scripts may not work.")
-    sendnotification("Unsupported executor detected, some scripts may not work.")
-end
+--------------------------------------------------------------------------------------DEFINE----------------------------------------------------------------------------------------
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/OtherScripts/VenyxUI.lua"))()
+local R3THLOADER = library.new("R3TH PRIV                                                                                .gg/pethicial", 5013109572)
+local Settings = R3THLOADER:addPage("Settings", 5012544372)
+local Settingss = Settings:addSection("Settings")
 
-request({
-    Url = 'http://127.0.0.1:6463/rpc?v=1',
-    Method = 'POST',
-    Headers = {
-        ['Content-Type'] = 'application/json',
-        Origin = 'https://discord.com'
-    },
-    Body = game:GetService("HttpService"):JSONEncode({
-        cmd = 'INVITE_BROWSER',
-        nonce = game:GetService("HttpService"):GenerateGUID(false),
-        args = {code = "pethicial"}
-    })
-})
+--------------------------------------------------------------------------------------SETTINGS----------------------------------------------------------------------------------------
 
-if game.PlaceId == (142823291 or 335132309 or 636649648) then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Murder%20Mystery%202.lua'))()
-elseif game.PlaceId == 7863262550 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/FOBLOX.lua'))()
-elseif game.PlaceId == 5244411056 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Bloxy%20Bingo.lua'))()
-elseif game.PlaceId == 8908228901 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/SharkBite%202.lua'))()
-elseif game.PlaceId == 6847090259 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Bulked%20Up.lua'))()
-elseif game.PlaceId == 12151453277 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Abyss%20World.lua'))()
-elseif game.PlaceId == 2693739238 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/THIEF%20LIFE%20Simulator.lua'))()
-elseif game.PlaceId == 3891618314 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Harbor%20Havoc.lua'))()
-elseif game.PlaceId == 13772394625 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Blade%20Ball.lua'))()
-elseif game.PlaceId == 14732610803 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Blade%20Ball.lua'))()
-elseif game.PlaceId == 4889315193 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Total%20Roblox%20Drama.lua'))()
-elseif game.PlaceId == 4939362930 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Total%20Roblox%20Drama.lua'))()
-elseif game.PlaceId == 4939362610 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Total%20Roblox%20Drama.lua'))()
-elseif game.PlaceId == 7172152072 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Total%20Roblox%20Drama.lua'))()
-elseif game.PlaceId == 1962086868 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Tower%20of%20Hell.lua'))()
-elseif game.PlaceId == 3582763398 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Tower%20of%20Hell.lua'))()
-elseif game.PlaceId == 5253186791 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Tower%20of%20Hell.lua'))()
-else
-    print("[ R3TH PRIV ]: Game not Supported.")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Universal.lua'))()
-end
+Settingss:addDropdown("Select Executor", {"Solara", "Codex", "Other"}, function(Value)
+    if Value == "Solara" then
+        getgenv().R3THEXECUTOR = "Unsupported"
+    else
+        getgenv().R3THEXECUTOR = "Supported"
+    end
+    writefile("R3TH PRIV/Executor.txt", (R3THEXECUTOR))
+end)
+
+Settingss:addButton("Launch R3TH PRIV", function()
+    if R3THEXECUTOR ~= nil then
+        R3THLOADER:toggle()
+        loadr3th()
+    else
+        sendnotification("To proceed, choose a executor.")
+    end
+end)
+
+R3THLOADER:SelectPage(R3THLOADER.pages[1], true)
